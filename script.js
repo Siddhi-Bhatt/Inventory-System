1c01bca5422b9c1eacc210c82ed6ff907c7e2c81
Siddhi-Bhatt <siddhitrisha03@gmail.com>
2024-11-30 21:15:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function searchResources() {
-    const searchTerm = document.querySelector(".search-container input[type='text']").value;
-    if (searchTerm) {
-        alert(`Searching for resources related to: ${searchTerm}`);
-    } else {
-        alert("Please enter a search term.");
-    }
-}
-
